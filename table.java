7a7687d2bfe8cc1134aaedeb7d014e08b68891cb
Yash Srivastava <yashshiv2006@gmail.com>
2026-08-25 23:03:44 +0530

    Fix table class and add user prompt for input

--- table.java
+++ table.java
@@ -1,9 +1,11 @@
 import java.util.*;
- class table{
+
+ public class table{
     public static void main(String arg[]){
+    System.out.print("Enter the Number whose table you want: ");
      Scanner sc = new Scanner(System.in);
      int table_of_num = sc.nextInt();
-     for(int i = 1 ; i <=10 ; 1++){
+     for(int i = 1 ; i <=10 ; i++){
       int table = table_of_num*i;
        System.out.println(table);
      }     
