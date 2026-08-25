d61bdaa136ebe6a89342d21b3400a9d91708f9d8
Yash Srivastava <yashshiv2006@gmail.com>
2026-08-25 22:58:14 +0530

    Update table.java

--- table.java
+++ table.java
@@ -1,7 +1,12 @@
 import java.util.*;
- class 16.table{
+ class table{
     public static void main(String arg[]){
-        Scanner sc = new Scanner(System.in);
+     Scanner sc = new Scanner(System.in);
+     int table_of_num = sc.nextInt();
+     for(int i = 1 ; i <=10 ; 1++){
+      int table = table_of_num*i;
+       System.out.println(table);
+     }     
 
     }
 }
