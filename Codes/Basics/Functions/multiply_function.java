ba4367e13f5827201d7922b77c3d34abb46f6690
yashshiv2006-web <yashshiv2006@gmail.com>
2026-08-30 23:46:45 +0530

    This gives you a number of the factorial

---
+++ Codes/Basics/Functions/multiply_function.java
@@ -0,0 +1,15 @@
+public class multiply_function{
+
+    public static void multiply(int A , int B){
+        int multiply = A*B;
+        System.out.println(" a = :"+A);
+        System.out.println(" b = :"+B);
+        System.out.println("The multiply :"+multiply);
+    }
+    public static void main(String Args[]){
+        int a = 13 ;
+        int b = 10 ;
+        multiply(a, b) ;
+
+    }
+}
